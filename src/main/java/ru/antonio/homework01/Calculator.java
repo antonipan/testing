7a2ba1ed0e1af6d8f184dbfce6ba7eb5c1026cf6
Anton P <a.s.panferov@mail.ru>
2023-09-24 16:28:27 +0300

    testing method of calculator sale sum

---
+++ src/main/java/ru/antonio/homework01/Calculator.java
@@ -0,0 +1,21 @@
+package ru.antonio.homework01;
+
+public class Calculator {
+    /**
+     * Метод рассчёта скидки исходя из суммы покупки
+     * @param summCount - сумма покупки
+     * @param sale - размер скидки
+     * @return - возвращается сумма покупки с учётом скидки
+     * @throws ArithmeticException - исключение, если сумма покупки или размер скидки не укладываются в указанный диапозон
+     */
+    public static double calculateDiscount (double summCount, double sale) throws ArithmeticException {
+        if (sale <= 0 || sale > 50) {
+            throw new ArithmeticException("Такой скидки не существует");
+        }
+        if (summCount < 10 || summCount > 1000) {
+            throw new ArithmeticException("Товара такой стоимости не существует");
+        }
+        return summCount - (summCount*sale/100);
+    }
+
+}
